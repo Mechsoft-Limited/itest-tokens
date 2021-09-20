ce09ec8666c0c93e44e7e96b2ecfe54b5ee0cc09
Elias Bundala <ebundala@gmail.com>
2021-09-20 14:10:23 +0300

    chore(migration) migrations

--- migrations/2_tokens_migrations.js
+++ migrations/2_tokens_migrations.js
@@ -1,3 +1,5 @@
+const fs = require('fs');
+const path = require('path');
 const totalSupply = 1000000;
 const token1= artifacts.require("Token1");
 const token2= artifacts.require("Token2");
@@ -8,4 +10,10 @@ module.exports = async function(deployer) {
   await deployer.link(safeMath, [token1, token2]);
   await deployer.deploy(token1, totalSupply);
   await deployer.deploy(token2, totalSupply);
+
+ const _token1 = await token1.deployed();
+ const _token2 = await token2.deployed();
+ fs.writeFileSync(path.join(__dirname,"../address1.txt"),_token1.address);
+ fs.writeFileSync(path.join(__dirname,"../address2.txt"),_token2.address);
+
 }
